0eda65474154e4eeefb05ce0280babcd8789cd06
Sem Bogaarts <sembogaarts@gmail.com>
2018-11-29 14:45:28 +0100

    View update

---
+++ src/main/java/com/tosad/brm/web/Main.java
@@ -0,0 +1,18 @@
+package com.tosad.brm.web;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("")
+public class Main {
+
+    @GET
+    @Path("")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String main() {
+        return "Awesome Vue.js page!";
+    }
+
+}
