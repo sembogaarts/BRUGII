4d67f0c2550ab1f5e02c050b0a79356e622fecf1
Robert-Jan Keizer <robertjankeizer@gmail.com>
2019-01-21 15:13:35 +0100

    Add hibernate

--- src/main/java/com/tosad/brg/api/Main.java
+++ src/main/java/com/tosad/brg/api/Main.java
@@ -1,13 +1,8 @@
 package com.tosad.brg.api;
 
-import com.tosad.brg.api.domain.businessRule.BusinessRuleTag;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class Main {
     SessionFactory factory;
