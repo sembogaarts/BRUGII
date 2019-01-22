f1c65257dcfdcb14475740f3799ade6cd60234c2
Sem Bogaarts <sembogaarts@gmail.com>
2019-01-22 15:10:31 +0100

    Added gradients

--- src/components/generator/generator.vue
+++ src/components/generator/generator.vue
@@ -1,13 +1,15 @@
 <template>
     <div>
 
-        <!-- Template Selector -->
-        <label for="templateSelector">TEMPLATE TYPE</label>
-        <select @change="getTemplateInformation()" v-model="selectedTemplate" name="templateSelector"
-                id="templateSelector">
-            <option disabled value="null" selected>Selecteer een template</option>
-            <option v-for="template in templates" :value="template.id">{{ template.name }}</option>
-        </select>
+        <div class="template-type-selector">
+            <!-- Template Selector -->
+            <label for="templateSelector">Selecteer een template type</label>
+            <select @change="getTemplateInformation()" v-model="selectedTemplate" name="templateSelector"
+                    id="templateSelector">
+                <option disabled value="null" selected>Selecteer een template</option>
+                <option v-for="template in templates" :value="template.id">{{ template.name }}</option>
+            </select>
+        </div>
 
         <!-- Template Generator -->
         <div v-if="hasTemplate">
@@ -26,7 +28,8 @@
                         <div v-for="(row, rowIndex) in getLoopRows(tag)" class="group">
                             <div v-for="(field, index) in row" class="groupinputbox">
                                 <!-- Render the field -->
-                                <loop :tag="field" :tags="template.tags" :loop="row" :rowIndex="rowIndex" :index="index" :schema="schema"></loop>
+                                <loop :tag="field" :tags="template.tags" :loop="row" :rowIndex="rowIndex" :index="index"
+                                      :schema="schema"></loop>
                             </div>
                         </div>
 
