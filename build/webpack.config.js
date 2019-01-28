30a2fc44ed4c78a4e67c9708a6887fbdf76667a4
Sem Bogaarts <sembogaarts@gmail.com>
2019-01-28 16:03:24 +0100

    Update

---
+++ build/webpack.config.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+    mode: 'development',
+    entry: [
+        './src/js/app.js',
+        './src/sass/theme.scss'
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ],
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+}
