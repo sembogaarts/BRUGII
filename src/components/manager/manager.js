a14e8bbb13031ffc3a83fdd63de2effdfad9bd9d
wesley <wesleyvanstr@outlook.com>
2019-01-28 14:19:41 +0100

    businessrules ophalen uit api

--- src/components/manager/manager.js
+++ src/components/manager/manager.js
@@ -1,7 +1,25 @@
 module.exports = {
     data: function() {
         return {
-            project: "Manager"
+            project: "Manager",
+            businessrules : [],
+            createStatus : ""
         }
+    },
+    methods: {
+        getBusinessRules() {
+            this.axios.get('http://localhost:8080/businessrule/rules')
+                .then(response => {
+                    this.businessrules = response.data;
+                });
+        },
+
+        generateBusinessRule(id) {
+            this.axios.get('http://localhost:8080/businessrule/generate?businessrule=' + id)
+                .then(response => {
+                    this.createStatus = response.data;
+                });
+        }
+
     }
 };
--- src/components/manager/manager.vue
+++ src/components/manager/manager.vue
@@ -13,23 +13,11 @@
                 <th class="iconth"> Generate</th>
                 </thead>
                 <tbody>
-                <tr>
-                    <td>brg_gebruikers_cns_arng_01</td>
-                    <td> Actief</td>
-                    <td class="icontd"><i class="far fa-edit"></i></td>
-                    <td class="icontd"><i class="fas fa-sync-alt"></i></td>
-                </tr>
-                <tr>
-                    <td> brg_gebruikers_cns_arng_02</td>
-                    <td> Inactief</td>
-                    <td class="icontd"><i class="far fa-edit"></i></td>
-                    <td class="icontd"><i class="fas fa-sync-alt"></i></td>
-                </tr>
-                <tr>
-                    <td> brg_gebruikers_cns_arng_03</td>
-                    <td> Actief</td>
-                    <td class="icontd"><i class="far fa-edit"></i></td>
-                    <td class="icontd"><i class="fas fa-sync-alt"></i></td>
+                <tr v-for="businessrule in businessrules">
+                    <td> {{businessrule.name}} </td>
+                    <td> {{businessrule.status}} </td>
+                    <td> <i class="far fa-edit"> </i> </td>
+                    <td> <i @click="generateBusinessRule(businessrule.id)" class="fas fa-sync-alt"></i> </td>
                 </tr>
                 </tbody>
             </table>
