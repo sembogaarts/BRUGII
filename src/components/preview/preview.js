8ea86284c127566f047de1e5705648dc1f3d1e46
wesley <wesleyvanstr@outlook.com>
2019-01-29 18:56:50 +0100

    preview fix

--- src/components/preview/preview.js
+++ src/components/preview/preview.js
@@ -2,14 +2,26 @@ module.exports = {
     props: ['businessrules'],
     data: function () {
         return {
-            templates: [{name:"brg_Gebruiker_cns_ACR", template: "ALTER TABLE suppliers ADD CONSTRAINT check_supplier_name CHECK (supplier_name IN ('IBM', 'Microsoft', 'NVIDIA'));"},
-                {name:"brg_Gebruiker_cns_ACR", template: "ALTER TABLE suppliers ADD CONSTRAINT check_supplier_name CHECK (supplier_name IN ('IBM', 'Microsoft', 'NVIDIA'));"}]
+            templates: [],
+            loading:false,
+            error: false,
         }
     },
     methods: {
-
+        getTemplates() {
+            this.loading = true;
+            this.error = false;
+            this.axios.post('https://brugii-generator.herokuapp.com/api/businessrule', this.businessrules)
+                .then(response => {
+                    this.templates = response.data;
+                    this.loading = false;
+                }, error => {
+                    this.error = true;
+                    this.loading = false;
+                });
+        }
     },
     created: function() {
-        console.log(this.businessrules);
+        this.getTemplates();
     }
 };
