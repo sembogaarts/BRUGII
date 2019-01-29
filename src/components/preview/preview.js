27b21aa46f5bf162a05d07e368738003e642a8d7
wesley <wesleyvanstr@outlook.com>
2019-01-29 15:13:47 +0100

    previes page update

--- src/components/preview/preview.js
+++ src/components/preview/preview.js
@@ -2,7 +2,8 @@ module.exports = {
     props: ['businessrules'],
     data: function () {
         return {
-            templates: []
+            templates: [{name:"brg_Gebruiker_cns_ACR", template: "ALTER TABLE suppliers ADD CONSTRAINT check_supplier_name CHECK (supplier_name IN ('IBM', 'Microsoft', 'NVIDIA'));"},
+                {name:"brg_Gebruiker_cns_ACR", template: "ALTER TABLE suppliers ADD CONSTRAINT check_supplier_name CHECK (supplier_name IN ('IBM', 'Microsoft', 'NVIDIA'));"}]
         }
     },
     methods: {
--- src/components/preview/preview.vue
+++ src/components/preview/preview.vue
@@ -3,7 +3,30 @@
     <div class="preview">
 
         <div v-for="template of templates">
+            <div class="card-wrapper">
+                <div class="card">
+                    <span class="card-header"> {{template.name}} </span>
+                </div>
+                <div v-if="loading" class="loader-wrapper">
+                    <i class="fas loader fa-sync fa-spin"></i>
+                </div>
 
+                <div v-if="error" class="loader-wrapper" @click="getBusinessRules()">
+                    <i class="fas loader fa-redo-alt"></i>
+                </div>
+
+                <table v-if="!loading && !error" class="table table-preview">
+                    <thead class="table-header">
+                        <th> CODE </th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td> <pre> <code> {{template.template}} </code></pre></td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
         </div>
 
     </div>
